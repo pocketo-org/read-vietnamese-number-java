7f381395b840afc9f0a83d2c4a8bf8ba41d54e7d
Vu Tong <tonghoangvu@outlook.com>
2023-12-10 22:17:41 +0700

    Init project

---
+++ src/main/java/org/pocketo/readvietnamesenumber/App.java
@@ -0,0 +1,13 @@
+package org.pocketo.readvietnamesenumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
